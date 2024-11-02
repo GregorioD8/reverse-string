6da79583bd4df228f54b6830bd20e4f6ff3af27f
Gregorio <Greg.del801@gmail.com>
2024-11-02 15:01:53 -0700

    completed exercise

--- solutions/reverse_string.py
+++ solutions/reverse_string.py
@@ -1,10 +1,21 @@
 def reverse_string(string):
+    """
+    Reverses the given string without using built in reverse methods
 
+    Parameters: 
+    string (str): The string to be reversed
+
+    Returns:
+    str: The reversed string.
+    """
+
+    # Initialize empty string to hold reversed characters
     reversed_string = ""
-
+    # Iterate over the input string from the last character to the first
     for i in range(len(string) - 1, -1, -1):
+        # Append each character from the end of the input string to the reversed string
         reversed_string += string[i]
-
+    # Return the final reversed string
     return reversed_string
 
 print("Expecting: 'ih'")
